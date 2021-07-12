5449615d65337da40b6bbad2b441befcc570c083
vinci-xiao <t107408001@ntut.org.tw>
2021-07-12 21:17:47 +0800

    add pui_msgs on micro_ros_arduino teensy32

--- pui_uwb/firmware/teensy_ros/teensy_ros.ino
+++ pui_uwb/firmware/teensy_ros/teensy_ros.ino
@@ -57,15 +57,6 @@ void error_loop(){
   }
 }
 
-//void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
-//{  
-//  RCLC_UNUSED(last_call_time);
-//  if (timer != NULL) {
-//    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-//    msg.data++;
-//  }
-//}
-
 void setup() 
 {
   set_microros_transports();
