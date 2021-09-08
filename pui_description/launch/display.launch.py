ff222169bcdea90e0eda3d72cf24d79d1044a3df
vinci-xiao <t107408001@ntut.orgt.tw>
2021-09-09 04:46:47 +0800

    sync pui_description

--- pui_description/launch/display.launch.py
+++ pui_description/launch/display.launch.py
@@ -8,7 +8,7 @@ def generate_launch_description():
     default_model_path = os.path.join(pkg_share, 'src/description/pui_description.urdf')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
     world_path=os.path.join(pkg_share, 'world/my_world.sdf')
-
+    
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -20,7 +20,6 @@ def generate_launch_description():
         name='joint_state_publisher',
         condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
-
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
         executable='rviz2',
@@ -28,20 +27,18 @@ def generate_launch_description():
         output='screen',
         arguments=['-d', LaunchConfiguration('rvizconfig')],
     )
-    
     spawn_entity = launch_ros.actions.Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
+    	package='gazebo_ros', 
+    	executable='spawn_entity.py',
         arguments=['-entity', 'pui_bot', '-topic', 'robot_description'],
         output='screen'
-        )
-
+    )
     robot_localization_node = launch_ros.actions.Node(
-       package='robot_localization',
-       executable='ekf_node',
-       name='ekf_filter_node',
-       output='screen',
-       parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+         package='robot_localization',
+         executable='ekf_node',
+         name='ekf_filter_node',
+         output='screen',
+         parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
     )
 
     return launch.LaunchDescription([
