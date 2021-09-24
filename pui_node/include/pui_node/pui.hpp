0efec0d6a34cceff128d786b40d29b90be8562db
vinci-xiao <t107408001@ntut.orgt.tw>
2021-09-25 05:00:30 +0800

    add dyanmixel wrapper

--- pui_node/include/pui_node/pui.hpp
+++ pui_node/include/pui_node/pui.hpp
@@ -1,19 +1,3 @@
-// Copyright 2019 ROBOTIS CO., LTD.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// Author: Darby Lim
-
 #ifndef PUI_NODE__PUI_HPP_
 #define PUI_NODE__PUI_HPP_
 
@@ -36,7 +20,7 @@
 #include <queue>
 
 // #include "pui_node/control_table.hpp"
-// #include "turtlebot3_node/dynamixel_sdk_wrapper.hpp"
+#include "pui_node/dynamixel_sdk_wrapper.hpp"
 #include "pui_node/odometry.hpp"
 
 // #include "turtlebot3_node/devices/devices.hpp"
