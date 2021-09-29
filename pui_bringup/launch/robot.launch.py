a6066a03b095fa5d2ff1af0cb819ed6bbbfc9012
vinci-xiao <t107408001@ntut.orgt.tw>
2021-09-29 12:54:32 +0800

    add for state_publisher

--- pui_bringup/launch/robot.launch.py
+++ pui_bringup/launch/robot.launch.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python3
-#
-# Copyright 2019 ROBOTIS CO., LTD.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Authors: Darby Lim
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -31,7 +13,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     # TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
 
-    # usb_port = LaunchConfiguration('usb_port', default='/dev/ttyACM0')
+    usb_port = LaunchConfiguration('usb_port', default='/dev/ttyUSB0')
 
     pui_param_dir = LaunchConfiguration(
         'pui_param_dir',
@@ -52,10 +34,10 @@ def generate_launch_description():
             default_value=use_sim_time,
             description='Use simulation (Gazebo) clock if true'),
 
-        # DeclareLaunchArgument(
-        #     'usb_port',
-        #     default_value=usb_port,
-        #     description='Connected USB port with OpenCR'),
+        DeclareLaunchArgument(
+            'usb_port',
+            default_value=usb_port,
+            description='Connected USB port with U2D2'),
 
         DeclareLaunchArgument(
             'pui_param_dir',
@@ -77,6 +59,6 @@ def generate_launch_description():
             package='pui_node',
             executable='pui_ros',
             parameters=[pui_param_dir],
-            # arguments=['-i', usb_port],
+            arguments=['-i', usb_port],
             output='screen'),
     ])
