aac4af21ae6ad395fa9cf2f813bf2d7fc26dd8cd
vinci-xiao <t107408001@ntut.orgt.tw>
2021-09-29 18:19:32 +0800

    add imu_link and update urdf

--- pui_bringup/launch/pui_state_publisher.launch.py
+++ pui_bringup/launch/pui_state_publisher.launch.py
@@ -3,7 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
@@ -12,6 +12,8 @@ def generate_launch_description():
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
     urdf_file_name = 'pui_bot' + '.urdf'
+    pkg_share = FindPackageShare(package='pui_description').find('pui_description')
+    default_model_path = os.path.join(pkg_share, 'urdf/pui_bot.urdf')
 
     print("urdf_file_name : {}".format(urdf_file_name))
 
@@ -30,13 +32,25 @@ def generate_launch_description():
     print (robot_desc) # Printing urdf information.
 
     return LaunchDescription([
-        DeclareLaunchArgument(
+        DeclareLaunchArgument
+        (
             'use_sim_time',
             default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
-        Node(
+            description='Use simulation (Gazebo) clock if true'
+        ),
+
+        DeclareLaunchArgument
+        (
+            name='model',
+            default_value=default_model_path,
+            description='Absolute path to robot urdf file'
+        ),
+
+        Node
+        (
             package='robot_state_publisher',
             executable='robot_state_publisher',
             output='screen',
-            parameters=[rsp_params, {'use_sim_time': use_sim_time}])
+            parameters=[rsp_params, {'use_sim_time': use_sim_time,'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+        )
     ])
