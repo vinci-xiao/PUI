4baa2650725d1ca0dc58a4b874bdfed247dbaee5
vinci-xiao <t107408001@ntut.orgt.tw>
2021-10-19 19:50:23 +0800

    update topic name

--- pui_uwb/launch/trilateration_mse.launch.py
+++ pui_uwb/launch/trilateration_mse.launch.py
@@ -12,5 +12,5 @@ def generate_launch_description():
 
         launch_ros.actions.Node(
             package='pui_uwb', executable='trilateration_mse', name='trilateration_mse',
-            remappings=[('/uwb_range', '/uwb_teensy_node_publisher')])
+            remappings=[('/uwb_range', '/uwb_range')])
     ])
