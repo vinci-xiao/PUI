1435d2bd134f87259d4a80d8c1ddbfae36012a37
vinci-xiao <t107408001@ntut.org.tw>
2022-07-15 20:52:10 +0800

    add odom_path but not working now

--- pui_markers/src/tag_path.cpp
+++ pui_markers/src/tag_path.cpp
@@ -6,13 +6,17 @@
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "std_msgs/msg/bool.hpp"
 #include "nav_msgs/msg/path.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 using namespace std::chrono_literals;
 
 class TagVISUAL : public rclcpp::Node
 {
 public:
-    nav_msgs::msg::Path path_record;
-    geometry_msgs::msg::PoseStamped pose_record;  
+    nav_msgs::msg::Path tag_record;
+    geometry_msgs::msg::PoseStamped tag_pose_record;  
+
+    nav_msgs::msg::Path odom_record;
+    geometry_msgs::msg::PoseStamped odom_pose_record;  
 
     TagVISUAL()
     : Node("tag_visual")
@@ -23,54 +27,62 @@ public:
         rst_subscription_ = this->create_subscription<std_msgs::msg::Bool>(
         "reset_tag_path", 1, std::bind(&TagVISUAL::reset_callback, this, std::placeholders::_1));
 
-        path_publisher_ = this->create_publisher<nav_msgs::msg::Path>("path_record", 3);
-        marker_publisher_ = this->create_publisher<visualization_msgs::msg::Marker>("tag_marker", 1);
+        odom_subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
+        "odom", 2, std::bind(&TagVISUAL::odom_callback, this, std::placeholders::_1));
 
-        timer_ = this->create_wall_timer(200ms, std::bind(&TagVISUAL::timer_callback, this));
+        tag_path_publisher_ = this->create_publisher<nav_msgs::msg::Path>("tag_path", 3);
+
+        odom_path_publisher_ = this->create_publisher<nav_msgs::msg::Path>("odom_path", 3);
+
+        // marker_publisher_ = this->create_publisher<visualization_msgs::msg::Marker>("tag_marker", 1);
+
+        // timer_ = this->create_wall_timer(200ms, std::bind(&TagVISUAL::timer_callback, this));
     }
 private:
     void tag_callback(geometry_msgs::msg::Pose::SharedPtr tag) 
     {
-      path_record.header.frame_id = "/base_link";
-      pose_record.header.frame_id = "/base_link";
-      pose_record.pose.position = tag->position;
-      pose_record.pose.orientation = tag->orientation;
-      path_record.poses.push_back(pose_record);
+      tag_record.header.frame_id = "world";
+      tag_pose_record.header.frame_id = "world";
+      tag_pose_record.pose.position = tag->position;
+      tag_pose_record.pose.orientation= tag->orientation;
+      tag_record.poses.push_back(tag_pose_record);
 
-      path_publisher_->publish(path_record);
+      tag_path_publisher_->publish(tag_record);
     }
+
     void reset_callback(std_msgs::msg::Bool::SharedPtr rst) 
     {
       if(rst->data == true)
-      path_record.poses.clear();
+      {
+        tag_record.poses.clear();
+        odom_record.poses.clear();
+      }
+      
     }
-    void timer_callback()
-    {
-      static uint32_t shape = visualization_msgs::msg::Marker::CYLINDER;
-      static visualization_msgs::msg::Marker tag_marker;
-      tag_marker.header.frame_id = "/base_link";
-      tag_marker.header.stamp = rclcpp::Clock().now();
-      tag_marker.ns = "tag_mark";
-      tag_marker.id = 0;
-      tag_marker.type = shape;
-      tag_marker.action = visualization_msgs::msg::Marker::ADD;
-      tag_marker.pose.position = pose_record.pose.position;          
-      tag_marker.pose.orientation = pose_record.pose.orientation; 
-      tag_marker.scale.x = 0.15;
-      tag_marker.scale.y = 0.15;
-      tag_marker.scale.z = 0.15;
-      tag_marker.color.r = 1.0f;
-      tag_marker.color.g = 0.0f;
-      tag_marker.color.b = 1.0f;
-      tag_marker.color.a = 1.0;
-      marker_publisher_->publish(tag_marker);
 
+    void odom_callback(nav_msgs::msg::Odometry::SharedPtr odom) 
+    {
+      odom_record.header.frame_id = "world";
+      odom_pose_record.header.frame_id = "world";
+      odom_pose_record.pose.position = odom->pose.pose.position;
+      // odom_pose_record.pose.orientation = odom->pose.pose.orientation;
+      odom_pose_record.pose.orientation.x=0.00;
+      odom_pose_record.pose.orientation.y=0.00;
+      odom_pose_record.pose.orientation.z=0.00;
+      odom_pose_record.pose.orientation.w=1.00;
+      odom_record.poses.push_back(odom_pose_record);
+
+      odom_path_publisher_->publish(odom_record);
     }
+
     rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr tag_pos_subscription_;
     rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr rst_subscription_;
-    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_publisher_;
-    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_publisher_;
-    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscription_;
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr tag_path_publisher_;
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr odom_path_publisher_;
+
+    // rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_publisher_;
+    // rclcpp::TimerBase::SharedPtr timer_;
 
 };
 
